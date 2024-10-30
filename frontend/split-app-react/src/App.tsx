43e67ec0e7924d4e1035a614c70eb5ef9e9ac76e
Tejas Koundinya <tejaskoundinya@gmail.com>
2024-10-30 01:30:28 -0700

    rename app (#8)

--- frontend/split-app-react/src/App.tsx
+++ frontend/split-app-react/src/App.tsx
@@ -27,7 +27,7 @@ const App: React.FC = () => {
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6" sx={{ flexGrow: 1 }}>
-            Splitwise Clone
+            Split App To Help Me
           </Typography>
           {currentUser ? (
             <>
